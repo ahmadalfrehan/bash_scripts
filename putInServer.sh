100928bd4aed256b50d1d8f097f801e95076b783
Ahmad Al_Frehan <81897367+ahmedalfrehan@users.noreply.github.com>
2022-12-16 09:04:30 -0800

    Create putInServer.sh

---
+++ putInServer.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+HOST="127.1"
+
+USER="ahmadalfrehan"
+
+PASSWORD="1111"
+
+DESTINATION=$1
+
+ALL_FILES="${@:2}"
+
+echo $DESTINATION 
+
+echo "Falseee"
+ftp -inv $HOST <<EOF
+user $USER $PASSWORD
+cd $DESTINATION
+
+mput *.$ALL_FILES $ALL_FILES
+
+bye
+
+EOF
