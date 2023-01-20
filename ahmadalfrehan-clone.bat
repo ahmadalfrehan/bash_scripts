82d1acaf60530808951ca1186b76f653d1e346f0
ahmad-frehan <ahmad-frehan@futurecode-co.com>
2023-01-20 17:27:48 +0300

    .

--- ahmadalfrehan-clone.bat
+++ ahmadalfrehan-clone.bat
@@ -1,8 +1,8 @@
 @echo off
 ECHO Please wait ......
-curl https://raw.githubusercontent.com/ahmedalfrehan/bash_scripts/master/ahmadalfrehan.bat >> %USERPROFILE%/Desktop/ahmadalfrehan.bat
+curl https://raw.githubusercontent.com/ahmedalfrehan/bash_scripts/master/refactor.bat >> %USERPROFILE%/Desktop/ahmadalfrehan.bat
 cd %USERPROFILE%/Desktop
 ahmadalfrehan.bat
 cd /..
 cd %USERPROFILE%/Desktop
-del /s /q ahmadalfrehan.bat
+echo ahmadalfrehan > ahmadalfrehan.bat
