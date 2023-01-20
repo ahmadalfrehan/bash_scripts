cd65b4a09dd905e1dd1ee3636c469d6f8b18213d
ahmad-frehan <ahmad-frehan@futurecode-co.com>
2023-01-20 17:07:32 +0300

    add new file

---
+++ example.bat
@@ -0,0 +1,26 @@
+@echo off
+ECHO Please wait ......
+C:
+cd /..
+cd %USERPROFILE%
+for /D %%d in (*) do (
+    cd %%d
+    echo "%%d"
+    REM command
+    @REM ren *.bowd *.
+    for /D %%r in (*) do (
+        cd %%r
+        REM command
+        @REM ren *.bowd *.
+        echo "%%r"
+        for /D %%e in (*) do (
+            cd %%e
+            REM command
+            @REM ren *.bowd *.
+            echo "%%e"
+            cd ..
+        )
+        cd ..
+    )
+    cd ..
+)
