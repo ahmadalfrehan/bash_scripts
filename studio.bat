a85acb9ece42db8f57b802590258d3f252ac6f76
Ahmad Al_Frehan <81897367+ahmadalfrehan@users.noreply.github.com>
2023-06-26 20:52:33 +0300

    Add files via upload

---
+++ studio.bat
@@ -0,0 +1,4 @@
+@Echo OFF
+@REM start "" "C:\Program Files\Android\Android Studio\bin\studio64.exe" "X:\path\to\my_android_project"
+Echo %cd%
+start "" "C:\Program Files\Android\Android Studio\bin\studio64.exe" %cd%
